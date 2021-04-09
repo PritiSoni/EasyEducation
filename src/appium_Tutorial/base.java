ac9caf2ba93fa2c2a6e86ea6bddc30d82064c08f
Priti <preetisoni1120@gmail.com>
2021-04-09 16:03:58 -0400

    My First Comment

---
+++ src/appium_Tutorial/base.java
@@ -0,0 +1,31 @@
+package appium_Tutorial;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.android.AndroidElement;
+
+
+public class base {
+
+	public static void main(String[] args) throws MalformedURLException {
+		// TODO Auto-generated method stub
+		File f = new File("src");
+		File fs = new File(f,"app-debug.apk");  //create file 
+		
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Priti_Emulator"); 
+		cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+		cap.setCapability(MobileCapabilityType.APP,fs.getAbsolutePath());
+		
+		
+		AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),cap);//connection to server
+		
+	}
+
+}
